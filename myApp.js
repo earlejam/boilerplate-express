50440f0f8c038c80289f356409e248c39efdafc6
James Earley <jamesearley22@gmail.com>
2025-03-14 18:29:15 -0500

    Register static assets to allow browser to GET stylesheet

--- myApp.js
+++ myApp.js
@@ -1,11 +1,14 @@
-const INDEX_FILE_PATH = __dirname + "/views/index.html"
+const INDEX_FILE_PATH = __dirname + '/views/index.html';
+const STATIC_ASSETS_FILE_PATH = __dirname + '/public';
 
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+console.log('Hello World');
 
-app.get("/", function(req, res) {
+app.use('/public', express.static(STATIC_ASSETS_FILE_PATH));
+
+app.get('/', function(req, res) {
     res.sendFile(INDEX_FILE_PATH);
 });
 
