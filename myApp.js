1c16e470692098914505f754cc2f2629a0d9b7f8
James Earley <jamesearley22@gmail.com>
2025-03-17 22:42:29 -0500

    Add /now endpoint

--- myApp.js
+++ myApp.js
@@ -30,6 +30,13 @@ app.get('/json', function(req, res) {
     res.json({"message": message});
 });
 
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({"time": req.time});
+});
+
 
 
 
